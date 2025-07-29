72d8b317bf1d6121753773776899c8dda7e134ef
Luiz Henrique Boger Wessling <57994263+Luiz1717@users.noreply.github.com>
2025-07-29 18:58:22 -0300

    Add files via upload

---
+++ BancoDigital.java
@@ -0,0 +1,15 @@
+public class BancoDigital {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("João Silva");
+
+        Conta cc = new ContaCorrente(cliente1);
+        Conta poupanca = new ContaPoupanca(cliente1);
+
+        cc.depositar(1000);
+        cc.transferir(300, poupanca);
+
+        cc.imprimirExtrato();
+        System.out.println();
+        poupanca.imprimirExtrato();
+    }
+}
---
+++ Conta.java
@@ -0,0 +1,48 @@
+public abstract class Conta {
+    protected static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato da Conta ===");
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Agência: " + agencia);
+        System.out.println("Número: " + numero);
+        System.out.printf("Saldo: R$ %.2f%n", saldo);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
